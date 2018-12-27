f4cf3219df3bf7fbedcb963d007958d9f7e41713
achuan <achuan@achuan.io>
2018-12-27 20:57:27 +0800

    删除了cnzz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,8 @@
-
 /*!--------------------------------*\
 	 3-Jekyll Theme
 	 @author Peiwen Lu (P233)
 	 https://github.com/P233/3-Jekyll
 \*---------------------------------*/
-
-// cnzz
-var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cspan id='cnzz_stat_icon_1256860805'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1256860805%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
 
 // Variables
 var sidebar    = $('#sidebar'),
@@ -142,32 +137,6 @@ function afterPjax() {
 			});
 		});
 	});
-
-	// Lazy Loading Disqus
-	// http://jsfiddle.net/dragoncrew/SHGwe/1/
-	// var ds_loaded = false,
-	//     top = $('#disqus_thread').offset().top;
-	// window.disqus_shortname = $('#disqus_thread').attr('name');
-	// function check() {
-	//   var currentScrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-	//   currentScrollTop = container.scrollTop();
-	//   if ( !ds_loaded && currentScrollTop + container.height() > top ) {
-	//     $.ajax({
-	//       type: 'GET',
-	//       url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
-	//       dataType: 'script',
-	//       cache: true
-	//     });
-	//     ds_loaded = true;
-	//   }
-	//   if (currentScrollTop > 400) {
-	//     $('#scroll-top').fadeIn(200);
-	//   } else {
-	//     $('#scroll-top').fadeOut(200);
-	//   }
-	// }
-	// check();
-	// container.scroll(check);
 
 	if (sidebar.hasClass('mobile')) {
 		$('#sidebar, #pjax, #icon-arrow').addClass('fullscreen');
