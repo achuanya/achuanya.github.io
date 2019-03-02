053d5506db3788ca6c13af33a2b52df14f4ddb0a
ahcuanya <achuan@achuan.io>
2019-03-02 23:26:51 +0800

    更新了留言板bug

--- assets/js/modalEffects.js
+++ assets/js/modalEffects.js
@@ -1,44 +1,19 @@
-// 背景遮盖
+背景遮盖
 var ModalEffects = (function() {
 	function init() {
 		// 留言板
 		var overlay = document.querySelector( '.md-overlay' );
-
 		[].slice.call( document.querySelectorAll( '.md-trigger' ) ).forEach( function( el, i ) {
-
-			var modal = document.querySelector( '#' + el.getAttribute( 'data-modal' ) ),
-				close = modal.querySelector( '.md-close' );
-
-			function removeModal( hasPerspective ) {
-				classie.remove( modal, 'md-show' );
-
-				if( hasPerspective ) {
-					classie.remove( document.documentElement, 'md-perspective' );
-				}
-			}
-			function removeModalHandler() {
-				removeModal( classie.has( el, 'md-setperspective' ) ); 
-			}
-
+            
 			el.addEventListener( 'click', function( ev ) {
                 classie.add( modal, 'md-show' );
                 // 留言板
 				overlay.removeEventListener( 'click', removeModalHandler );
 				overlay.addEventListener( 'click', removeModalHandler );
 
-				if( classie.has( el, 'md-setperspective' ) ) {
-					setTimeout( function() {
-						classie.add( document.documentElement, 'md-perspective' );
-					}, 25 );
-				}
 			});
 
-			close.addEventListener( 'click', function( ev ) {
-				ev.stopPropagation();
-				removeModalHandler();
-			});
-
-		} );
+		});
 
 	}
 
